639b04dfb1d34bdb793bc5251a902363764f8736
chitienhsiehwork-ai <chitienhsieh.work@gmail.com>
2026-08-09 17:02:55 +0800

    Fix LeetCode completion toggle scope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,18 +3,17 @@
 -- Add any additional keymaps here
 
 -- LeetCode mode: disable completion suggestions (no cheating!)
-vim.g.leetcode_mode = false
 vim.keymap.set("n", "<leader>lc", function()
-  vim.g.leetcode_mode = not vim.g.leetcode_mode
+  vim.b.leetcode_mode = not vim.b.leetcode_mode
 
-  if vim.g.leetcode_mode then
+  if vim.b.leetcode_mode then
     vim.b.completion = false -- Disable blink for current buffer
-    vim.notify("LeetCode Mode ON - Completion disabled!", vim.log.levels.WARN)
+    vim.notify("LeetCode Mode ON - Completion disabled for current buffer!", vim.log.levels.WARN)
   else
     vim.b.completion = nil -- Re-enable blink
-    vim.notify("LeetCode Mode OFF - Completion enabled", vim.log.levels.INFO)
+    vim.notify("LeetCode Mode OFF - Completion enabled for current buffer", vim.log.levels.INFO)
   end
-end, { desc = "Toggle LeetCode mode (disable completion)" })
+end, { desc = "Toggle completion for current buffer" })
 
 -- Notification history (for copying error messages to debug)
 vim.keymap.set("n", "<leader>nt", function()
